4d81afff9ba61410eaa8ea15ad2fbf87d0bf1583
Hector Rivas Gandara <keymon@gmail.com>
2015-11-24 23:53:31 +0000

    Fix bug in .tf deps

--- bosh/aws/aws-vpc.tf
+++ bosh/aws/aws-vpc.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "default" {
-  depends_on = "aws_s3_bucket.terraform_state"
+  depends_on = [ "aws_s3_bucket.terraform_state" ]
   cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
   tags {
