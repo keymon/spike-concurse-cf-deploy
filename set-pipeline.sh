e2bb9c752e059bc38133d18c30df76d62a7f6d61
Hector Rivas Gandara <keymon@gmail.com>
2015-11-26 11:19:46 +0000

    Smart looking for flies

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -5,18 +5,26 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
+# Find fly binary
+[ -x ./fly ] && fly=./fly || fly=fly
+
+
 DEPLOY_ENV=$1; shift
 PIPELINE_YML=$1; shift
 
 
 PIPELINE_NAME=${PIPELINE_YML%%.yml}
 PIPELINE_NAME=${PIPELINE_NAME##*/}
-./fly set-pipeline -c $PIPELINE_YML \
+$fly set-pipeline -c $PIPELINE_YML \
     --var "deploy_env=$DEPLOY_ENV" \
     --var "bucket_name=${DEPLOY_ENV}-tfstate" \
     --var "aws_access_key_id=$AWS_ACCESS_KEY_ID" \
     --var "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" \
+    --var "docker_email=$DOCKER_EMAIL" \
+    --var "docker_user=$DOCKER_USER" \
+    --var "docker_pass=$DOCKER_PASS" \
+    --var "docker_repo_terraform=$DOCKER_USER/terraform" \
     -p $PIPELINE_NAME -n
 
-./fly unpause-pipeline -p $PIPELINE_NAME
+$fly unpause-pipeline -p $PIPELINE_NAME
 
